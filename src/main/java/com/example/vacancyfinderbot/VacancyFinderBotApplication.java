112cd04c0e60fe938b9e7084abbaa00e7a546e96
Ulug'bek <kimsanov0717@gmail.com>
2024-03-21 18:12:53 +0500

    Create

---
+++ src/main/java/com/example/vacancyfinderbot/VacancyFinderBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacancyfinderbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacancyFinderBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacancyFinderBotApplication.class, args);
+    }
+
+}
